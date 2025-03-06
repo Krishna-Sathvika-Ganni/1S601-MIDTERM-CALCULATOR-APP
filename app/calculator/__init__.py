1c10b0d317e4052fee58c01525842250181cb4bc
Krisa29 <sathvika.ganni@gmail.com>
2025-03-06 10:20:07 -0500

    Created class calculator and defined add function

---
+++ app/calculator/__init__.py
@@ -0,0 +1,20 @@
+from app.operation.operations import add # Imported add
+from app.calculator.calculation import Calculation
+from app.calculator.calculations import Calculations
+from decimal import Decimal
+from typing import Callable
+
+class Calculator:
+    '''Created the class Calculator'''
+
+    '''Used Static method'''
+
+    @staticmethod
+    def _perform_operation(x: Decimal, y: Decimal, operation: Callable[[Decimal, Decimal], Decimal]) -> Decimal:
+        calculation=Calculation.create(x, y, operation)
+        Calculations.add_calculation(calculation)
+        return calculation.perform()
+
+    @staticmethod
+    def add(x: Decimal,y: Decimal) -> Decimal:
+        return Calculator._perform_operation(x, y, add)
