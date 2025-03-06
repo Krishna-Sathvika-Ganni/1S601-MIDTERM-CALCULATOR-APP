8c26b7a7f8dff734bf3ca7b24a75f91371d2db3b
Krisa29 <sathvika.ganni@gmail.com>
2025-03-06 15:55:25 -0500

    Added function to test division in test_calculator file

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -26,4 +26,12 @@ def test_multiplication():
     expected=x*y
     assert Calculator.multiply(x,y)==expected
 
+def test_division():
+    '''Test that Division function works'''
+    x=fake.random_int(min=0,max=50)
+    y=fake.random_int(min=1,max=50)
+    y=y if y!=0 else 1
+    expected=x/y
+    assert Calculator.divide(x,y)==expected
+
 # End of program
