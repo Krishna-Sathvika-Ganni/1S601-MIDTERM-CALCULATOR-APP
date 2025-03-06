2ca9793cb479880c0a33b4425f38ca8e6334ec9e
Krisa29 <sathvika.ganni@gmail.com>
2025-03-06 12:08:42 -0500

    Modified app/__init__.py file  with logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,13 @@ import inspect
 import pkgutil
 import importlib
 from app.commands import CommandHandler, Command
+import logging
+import sys
+import os
+import logging
+import logging.config
+
+logger=logging.getLogger(__name__)
 
 class App:
     '''This is the App class'''
@@ -11,6 +18,16 @@ class App:
             command = CommandHandler()
         self.command_handler = command
         self.load_plugins()  # This automatically loads the plugins
+        os.makedirs('logs',exist_ok=True)
+        self.configuring_logging()
+
+    def configuring_logging(self):
+        logging_conf_path='logging.conf'
+        if os.path.exists(logging_conf_path):
+            logger.config.fileConfig(logging_conf_path, disable_existing_loggers=False)
+        else:
+            logger.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+            logger.info("Logging configured")
 
     def load_plugins(self):
         '''This dynamically loads the plugins'''
@@ -18,6 +35,7 @@ class App:
         for _, plugin_name, _ in pkgutil.iter_modules([plugins_package.replace(".","/")]):  
             try:
                 plugin_module = importlib.import_module(f"{plugins_package}.{plugin_name}")
+                logger.info("Attempting to load plugin")
                 for item_name in dir(plugin_module):
                     item = getattr(plugin_module, item_name)
                     if isinstance(item, type) and issubclass(item, Command) and item is not Command:
@@ -31,6 +49,7 @@ class App:
             
             except Exception as e:
                 print(f"Failed to load plugin {plugin_name}: {e}")
+                logger.error("Failed to load plugin")
 
     def start(self):
         print("WELCOME TO THE CALCULATOR PROGRAM!\n Type 'Menu' to see available commands. \n Type 'Exit' to quit.")
@@ -40,6 +59,7 @@ class App:
                 c = input("Enter the operation name: ").strip().lower()
                 if c == "exit":
                     raise SystemExit("Program is Exiting..!")
+                    logger.info("Exited")
                 
                 user_input_split = c.split()
                 command_name = user_input_split[0]
@@ -48,6 +68,8 @@ class App:
                 command_name=command_name.lower()
                 if not self.command_handler.Execute_Command(command_name, *args):
                     print(f"Error: '{command_name}' is not a registered command")
+                    logger.warning("Command not recognized")
             
             except Exception as e:
                 print(f"An error occurred: {e}")
+                logger.error("Error")
