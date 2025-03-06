9a7ab33b1ea9460a849a0aacd1680a23ebaa584f
Krisa29 <sathvika.ganni@gmail.com>
2025-03-06 10:34:55 -0500

    Added the code to test the Menu command

---
+++ tests/test_menucommand.py
@@ -0,0 +1,36 @@
+# pylint: disable=missing-class-docstring
+'''This is test_command file'''
+from unittest.mock import Mock, patch
+import pytest
+from app.plugins.menucommand import Menu
+
+
+@pytest.fixture
+def mock_command_handler():
+    '''Mock_command_handler'''
+    mock_handler=Mock()
+    mock_handler.Register_Command=Mock()
+    mock_handler.Execute_Command=Mock()
+    return mock_handler
+
+@pytest.fixture
+def menu_command(mock_command_handler):
+    '''Fixture for Menu Command'''
+    # Correct the MockCommandHandler to return the list of commands
+    mock_command_handler.Get_Registered_Commands = Mock(return_value=["Add", "Subtract", "Multiply", "Divide", "Menu"])
+    return Menu(command_handler=mock_command_handler)
+
+# Testing menu command
+
+class TestMenuCommand:
+    '''Test the Menu command'''
+    def test_menu_command(self, menu_command, capsys):
+        '''Test that the Menu command displays the list of available commands.'''
+        menu_command.execute()
+        captured = capsys.readouterr()
+        assert "Commands Available:" in captured.out, "MenuCommand should display the available commands"
+        assert "Add" in captured.out
+        assert "Subtract" in captured.out
+        assert "Multiply" in captured.out
+        assert "Divide" in captured.out
+        assert "Menu" in captured.out
