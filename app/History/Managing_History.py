00e51cbc5790216f1fc20c805530a5ac822d4aae
Krisa29 <sathvika.ganni@gmail.com>
2025-03-06 17:57:18 -0500

    Modified clear history

--- app/History/Managing_History.py
+++ app/History/Managing_History.py
@@ -42,9 +42,14 @@ class History_Manager:
 
     @classmethod
     def clear_history(cls):
+        if not Calculations.get_history():
+            logger.warning("No in-memory history to clear.")
+            print("No in-memory history to clear.")
+            return
+        
         Calculations.clear_history()
-        logger.info("Cleared History")
-        print("History cleared.")
+        logger.info("Cleared in-memory history.")
+        print("In-memory history cleared.")
 
     @classmethod
     def delete_history(cls):
