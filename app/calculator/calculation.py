c47e357c88dd433ccee2d457be487f2078182dec
Krisa29 <sathvika.ganni@gmail.com>
2025-03-06 10:18:35 -0500

    Created Calculation class and defined the functions

---
+++ app/calculator/calculation.py
@@ -0,0 +1,22 @@
+from app.operation.operations import add # Imported the add operation from operations 
+from decimal import Decimal
+from typing import Callable
+
+class Calculation:
+    '''Created a class calculation'''
+    def __init__(self, x: Decimal, y: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+        self.x = x
+        self.y = y
+        self.operation = operation
+
+    '''Used static method to define a method within a class'''
+    
+    @staticmethod
+    def create(x: Decimal, y: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+        return Calculation(x, y, operation)
+    
+    def perform(self) -> Decimal:
+        return self.operation(self.x, self.y)
+    
+    def __repr__(self):
+        return f"Calculation({self.x}, {self.y}, {self.operation.__name__})"
