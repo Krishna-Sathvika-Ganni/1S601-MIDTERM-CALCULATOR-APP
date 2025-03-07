bb3acced8e19be190ecb82efbeffab2fdcfed17b
Krisa29 <sathvika.ganni@gmail.com>
2025-03-06 20:57:31 -0500

    Mentioned where I have used LBYL ( Look Before You Leap ) condition

--- app/operation/operations.py
+++ app/operation/operations.py
@@ -9,6 +9,7 @@ def subtract(x: Decimal,y: Decimal) -> Decimal:
 def multiply(x: Decimal,y: Decimal) -> Decimal:
     return x * y
 
+# Here LBYL(Look Before You Leap condition ) is applied: we check whether the second number is zero or not before dividing
 def divide(x: Decimal,y: Decimal) -> Decimal:
     if y == Decimal('0'):
         raise ValueError("Cannot be divided by Zero")
